575e51391fdccdd7d0834f1d12e2f567f4869e0c
XackiGiFF <m.girsanoff@gmail.com>
2023-06-11 02:04:10 +0300

    Update DefRank.php
    Add with support CortexPE/Commando pm5

--- src/_64FF00/PurePerms/Commands/DefRank.php
+++ src/_64FF00/PurePerms/Commands/DefRank.php
@@ -1,89 +1,84 @@
 <?php
 
-namespace _64FF00\PurePerms\Commands;
+namespace XackiGiFF\MPEPerms\cmd;
 
-use _64FF00\PurePerms\PurePerms;
-
-use pocketmine\command\Command;
+use XackiGiFF\MPEPerms\MPEPerms;
+use XackiGiFF\MPEPerms\permissions\MPEPermsPermissions;
 use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseCommand;
+use CortexPE\Commando\args\RawStringArgument;
 use pocketmine\plugin\Plugin;
-use pocketmine\plugin\PluginOwned;
-use pocketmine\plugin\PluginOwnedTrait;
 use pocketmine\utils\TextFormat;
 
-class DefRank extends Command implements PluginOwned
-{
-	use PluginOwnedTrait;
-    /*
-        PurePerms by 64FF00 (Twitter: @64FF00)
 
-          888  888    .d8888b.      d8888  8888888888 8888888888 .d8888b.   .d8888b.
-          888  888   d88P  Y88b    d8P888  888        888       d88P  Y88b d88P  Y88b
-        888888888888 888          d8P 888  888        888       888    888 888    888
-          888  888   888d888b.   d8P  888  8888888    8888888   888    888 888    888
-          888  888   888P "Y88b d88   888  888        888       888    888 888    888
-        888888888888 888    888 8888888888 888        888       888    888 888    888
-          888  888   Y88b  d88P       888  888        888       Y88b  d88P Y88b  d88P
-          888  888    "Y8888P"        888  888        888        "Y8888P"   "Y8888P"
-    */
+class DefGroup extends BaseCommand {
+	/*
+		MPEPerms by XackiGiFF (Remake by @mpe_coders from PurePerms by #64FF00)
 
-    private $plugin;
+		╔═╗╔═╗╔═══╗╔═══╗     ╔═══╗╔═══╗╔═══╗╔═╗╔═╗╔═══╗
+		║║╚╝║║║╔═╗║║╔══╝     ║╔═╗║║╔══╝║╔═╗║║║╚╝║║║╔═╗║
+		║╔╗╔╗║║╚═╝║║╚══╗     ║╚═╝║║╚══╗║╚═╝║║╔╗╔╗║║╚══╗
+		║║║║║║║╔══╝║╔══╝     ║╔══╝║╔══╝║╔╗╔╝║║║║║║╚══╗║
+		║║║║║║║║───║╚══╗     ║║───║╚══╗║║║╚╗║║║║║║║╚═╝║
+		╚╝╚╝╚╝╚╝───╚═══╝     ╚╝───╚═══╝╚╝╚═╝╚╝╚╝╚╝╚═══╝
+	*/
 
-    /**
-     * @param PurePerms $plugin
-     * @param $name
-     * @param $description
-     */
-    public function __construct(PurePerms $plugin, $name, $description)
-    {
-        $this->plugin = $plugin;
-        parent::__construct($name, $description);
-        $this->setPermission("pperms.command.defrank");
-    }
+	protected const ARGUMENT_GROUP_NAME = "group_name";
+	protected const ARGUMENT_WORLD_NAME = "world_name";
 
-    /**
-     * @param CommandSender $sender
-     * @param $label
-     * @param array $args
-     * @return bool
-     */
-    public function execute(CommandSender $sender, string $label, array $args) : bool
-    {
-        if(!$this->testPermission($sender))
-            return false;
+	protected function prepare(): void {
+		// This is where we'll register our arguments and subcommands
 
-        if(!isset($args[0]) || count($args) > 2)
-        {
-            $sender->sendMessage(TextFormat::GREEN . PurePerms::MAIN_PREFIX . ' ' . $this->plugin->getMessage("cmds.defrank.usage"));
-            return true;
+		$this->setPermission(MPEPermsPermissions::COMMAND_DEFGROUP_PERMISSION);
+		try {
+			$this->registerArgument(0, new RawStringArgument(DefGroup::ARGUMENT_GROUP_NAME));
+            $this->registerArgument(1, new RawStringArgument(DefGroup::ARGUMENT_WORLD_NAME, true));
+        } catch (Exception) {
         }
+		$this->setErrorFormat(0x01, TextFormat::GREEN . MPEPerms::MAIN_PREFIX . ' ' . $this->getOwningPlugin()->getMessage("cmds.defgroup.usage"));
+		$this->setErrorFormat(0x02, TextFormat::GREEN . MPEPerms::MAIN_PREFIX . ' ' . $this->getOwningPlugin()->getMessage("cmds.defgroup.usage"));
+		$this->setErrorFormat(0x03, TextFormat::GREEN . MPEPerms::MAIN_PREFIX . ' ' . $this->getOwningPlugin()->getMessage("cmds.defgroup.usage"));
 
-        $group = $this->plugin->getGroup($args[0]);
-        if($group === null)
-        {
-            $sender->sendMessage(TextFormat::RED . PurePerms::MAIN_PREFIX . ' ' . $this->plugin->getMessage("cmds.defrank.messages.rank_not_exist", $args[0]));
-            return true;
-        }
-        $WorldName = null;
-        if(isset($args[1]))
-        {
-            $World = $this->plugin->getServer()->getWorldManager()->getWorldByName($args[1]);
-            if($World === null)
-            {
-                $sender->sendMessage(TextFormat::RED . PurePerms::MAIN_PREFIX . ' ' . $this->plugin->getMessage("cmds.defrank.messages.level_not_exist", $args[1]));
-                return true;
-            }
+	}
 
-            $WorldName = $World->getDisplayName();
-        }
-        $this->plugin->setDefaultGroup($group, $WorldName);
-        $sender->sendMessage(TextFormat::GREEN . PurePerms::MAIN_PREFIX . ' ' . $this->plugin->getMessage("cmds.defrank.messages.defrank_successfully", $args[0]));
-        
-        return true;
-    }
-    
-    public function getPlugin() : Plugin
-    {
-        return $this->plugin;
-    }
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+		// This is where the processing will occur if it's NOT handled by other subcommands
+		if(!$this->testPermission($sender)){
+			return;
+		}
+
+		$group_name = $this->getOwningPlugin()->getGroup($args["group_name"]);
+		$world_name = $args["world_name"];
+
+
+		if($group_name === null){
+			$sender->sendMessage(TextFormat::RED . MPEPerms::MAIN_PREFIX . ' ' . $this->getOwningPlugin()->getMessage("cmds.defgroup.messages.group_not_exist", [$args["group_name"]]));
+
+			return;
+		}
+
+		$levelName = null;
+
+		if(isset($world_name)){
+			$level = $this->getOwningPlugin()->getServer()->getWorldManager()->getWorldByName($world_name);
+
+			if($level === null){
+				$sender->sendMessage(TextFormat::RED . MPEPerms::MAIN_PREFIX . ' ' . $this->getOwningPlugin()->getMessage("cmds.defgroup.messages.level_not_exist", [$args["world_name"]]));
+
+				return;
+			}
+
+			$levelName = $level->getFolderName();
+		}
+
+		$this->getOwningPlugin()->setDefaultGroup($group_name, $levelName);
+
+		$sender->sendMessage(TextFormat::GREEN . MPEPerms::MAIN_PREFIX . ' ' . $this->getOwningPlugin()->getMessage("cmds.defgroup.messages.defgroup_successfully", [$args["group_name"]]));
+
+		return;
+	}
+
+	public function getPlugin() : Plugin{
+		return $this->getOwningPlugin();
+	}
 }
